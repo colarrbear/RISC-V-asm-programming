798cd9425c592fa3bedcf6b41ac6cab11840e05f
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-03-06 15:56:01 +0700

    fix2

--- dot_product_recursive.s
+++ dot_product_recursive.s
@@ -14,6 +14,13 @@ main:
 dot_product_iterative:
     addi t5, x0, 0                  # initialize result to 0
 
+    # Prepare Stack Pointer
+    addi sp, sp, -16
+    sw ra, 0(sp) # Save ra
+    sw a0, 4(sp)  # Save a0
+    sw a1, 8(sp)  # Save a1
+    sw a2, 12(sp) # Save a2
+
 # loop to compute dot product
 dot_product_loop:
     # load current elements of a and b
@@ -32,11 +39,19 @@ dot_product_loop:
     # check if loop should continue
     bnez a2, dot_product_loop
 
-    # exit loop if size becomes 0
+    # Restore Stack
+    lw a2, 12(sp)
+    lw a1, 8(sp)
+    lw a0, 4(sp)
+    lw ra, 0(sp)
+    addi sp, sp, 16
 
+    # Exit loop if size becomes 0
     jr ra
 
 exit:
+    mv t0, a0
+
     # print the dot product
     addi a0, x0, 4
     la a1, print_text
